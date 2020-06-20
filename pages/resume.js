26dc006fdf84f699bfd2d513fc52bffe7a5e28b3
Ian Buena <ianbuena081191@gmail.com>
2020-06-20 20:55:20 +0800

    Made the react-pdf work

---
+++ pages/resume.js
@@ -0,0 +1,45 @@
+import dynamic from 'next/dynamic'
+import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+
+const DynamicComponentWithNoSSR = dynamic(
+  () => import('../components/PDFViewer'),
+  { ssr: false }
+)
+
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: 'row',
+    backgroundColor: '#E4E4E4'
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1
+  }
+});
+
+// Create Document Component
+const Resume = (_props) => {
+  return (
+    <DynamicComponentWithNoSSR>
+      <Document>
+        <Page size="LETTER" style={styles.page}>
+          <View style={styles.section}>
+            <Text>Section #1</Text>
+          </View>
+          <View style={styles.section}>
+            <Text>Section #2</Text>
+          </View>
+        </Page>
+      </Document>
+    </DynamicComponentWithNoSSR>
+  )
+}
+
+export async function getServerSideProps(_context) {
+  return {
+    props: {},
+  }
+}
+
+export default Resume
