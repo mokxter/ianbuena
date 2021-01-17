ac8b23a37ebe2f445ac05acf76952d96a8a3af2f
Ian Cristopher Buena <ianbuena081191@gmail.com>
2021-01-17 15:03:06 +0800

    Add additional content

--- pages/index.vue
+++ pages/index.vue
@@ -1,51 +1,40 @@
 <template>
   <div class="container">
-    <div>
-      <h1 class="title">Ian Buena</h1>
-    </div>
+    <section>
+      <h3 class="introduction">Hello, I am</h3>
+      <h1 class="name">{{ title }}</h1>
+      <span class="profession">Web Developer</span>
+    </section>
+    <section></section>
   </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({})
+<script>
+export default {
+  data() {
+    return {
+      title: 'Ian Cristopher Buena',
+    }
+  },
+}
 </script>
 
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
+<style lang="postcss" scoped>
 .container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  @apply min-h-screen flex items-center text-left mx-auto;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
+.introduction {
+  @apply block text-4xl;
+}
+
+.name {
+  @apply block font-extralight text-7xl;
   color: #35495e;
   letter-spacing: 1px;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.profession {
+  @apply block font-extralight text-3xl;
 }
 </style>
